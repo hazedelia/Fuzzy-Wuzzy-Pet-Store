c34bc6338ee9621fb9e68e319fc61b67929c5fd6
ZaigaK <zaiga.kluine@gamil.com>
2020-06-21 12:01:45 +0300

    added DogController

---
+++ app/Http/Controllers/DogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Items;
+
+class DogController extends Controller
+{
+    public function index()
+    {
+        $items = Items::orderBy('title', 'asc')->where('category', '=', 'DOG')->get();
+        return view('items')->with('items', $items);
+    }
+}
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('');
 
 Route::get('/kakiem', 'CatController@index')->name('kakiem');
-Route:: get ('/suniem', 'ItemsController@index')->name('suniem');
-Route:: get ('/grauzejiem', 'ItemsController@index')->name('grauzejiem');
+Route:: get ('/suniem', 'DogController@index')->name('suniem');
+Route:: get ('/grauzejiem', 'ItemsController@o')->name('grauzejiem');
 
